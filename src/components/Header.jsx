6b124299025a2bd4cd5bf0f0dbe46f4e0190e514
javid shabin <javid.main.act@gmail.com>
2024-06-27 13:40:23 +0530

    styled the header sectiond and small screen responsiveness and add toggle i con

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,20 +4,24 @@ import { logo } from '../assets'
 function Header() {
   return (
     <>
-        <header>
-            <div className="container">
-                <div className="logo">
-                    <span><img src={logo} /><b>Groce</b></span>
+        <header className='flex justify-center items-center w-full h-[90px] shadow-lg'>
+            <div className="container w-[90%] flex justify-between items-center">
+                <div className="logo flex items-center">
+                <img src={logo} className='w-10'/>
+                    <span><b>Groce</b></span>
                 </div>
-                <ul>
+                <ul className='hidden md:flex gap-8'>
                     <li><a href="#">Home</a></li>
                     <li><a href="#">About</a></li>
                     <li><a href="#">Items</a></li>
                     <li><a href="#">Contact</a></li>
                 </ul>
-                <div className="icons">
+                <div className="icons hidden md:flex gap-5 text-xl">
                     <i class="ri-user-fill"></i>
                     <i class="ri-shopping-cart-2-fill"></i>
+                </div>
+                <div className="toggle-icon text-2xl font-semibold md:hidden">
+                    <i class="ri-menu-fill cursor-pointer"></i>
                 </div>
             </div>
         </header>
