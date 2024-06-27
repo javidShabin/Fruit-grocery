9ad376bb05a0509a9edd7f5c8fd0290d3bbb7cee
javid shabin <javid.main.act@gmail.com>
2024-06-27 13:21:55 +0530

    Add contents in header component and install remix icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
+import { logo } from '../assets'
 
 function Header() {
   return (
-    <div>
-      
-    </div>
+    <>
+        <header>
+            <div className="container">
+                <div className="logo">
+                    <span><img src={logo} /><b>Groce</b></span>
+                </div>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Items</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+                <div className="icons">
+                    <i class="ri-user-fill"></i>
+                    <i class="ri-shopping-cart-2-fill"></i>
+                </div>
+            </div>
+        </header>
+    </>
   )
 }
 
