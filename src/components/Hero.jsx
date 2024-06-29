a638517e07b823a08019b96e92841f97cbad98fc
javid shabin <javid.main.act@gmail.com>
2024-06-29 10:02:56 +0530

    make responsiveness for small screen

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -10,19 +10,19 @@ export default function Hero() {
     >
       <div className="container flex justify-between items-center overflow-hidden">
         <div className="left ml-10 mt-10">
-          <h1 className="text-[#60b8fc] text-[60px]">
+          <h1 className="text-[#60b8fc] text-[35px] text-center md:text-[60px]">
             Dont miss our daily <br /> amazing deal
           </h1>
-          <p className="text-[#7f7f7f] text-[20px]">
+          <p className="text-[10px]text-[#7e7e7e] md:text-[20px] text-[#7f7f7f] text-center">
             Save up to 60% off on your first order
           </p>
-          <div>
-            <input type="text" />
-            <button>Search</button>
+          <div className="bg-[#fff] w-[300px]">
+            <input type="text" className="w-[80%] md:w-[90%] border-0 outline-0" />
+            <button className="bg-[#60b8fc]">Search</button>
           </div>
         </div>
-        <div className="right">
-          <img src={heroImg} className="w-[37rem]" />
+        <div className="">
+          <img src={heroImg} className="hidden md:flex wh-[37rem]" />
         </div>
       </div>
     </div>
