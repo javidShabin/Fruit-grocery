d265538266cc213d44ea96f58c8c6505863f4536
javid shabin <javid.main.act@gmail.com>
2024-07-10 11:22:04 +0530

    Add border and correctly alignment in the project

--- src/components/Fproducts.jsx
+++ src/components/Fproducts.jsx
@@ -4,9 +4,11 @@ export default function Fproducts(props) {
   const { images, productName } = props;
   return (
     <>
-      <div className="wrapper">
-        <img className="w-[150px]" src={images} alt="" />
-        <h3>{productName}</h3>
+      <div className="wrapper border flex justify-center h-[200px] mt-10 ">
+        <div className="">
+          <img className="w-[150px]" src={images} alt="" />
+          <h3>{productName}</h3>
+        </div>
       </div>
     </>
   );
