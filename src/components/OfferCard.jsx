4d0bc97f09f2977ab953425deb9107e9139136cc
javid shabin <javid.main.act@gmail.com>
2024-07-11 20:40:02 +0530

    Add immages for offer card

--- src/components/OfferCard.jsx
+++ src/components/OfferCard.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from "react";
+import { offer1, offer2 } from "../assets";
 
 export default function OfferCard() {
   return (
     <div>
-      <h1>Offer Card section</h1>
+      <div className="container">
+        <div>
+            <img src={offer1} alt="" />
+        </div>
+        <div>
+            <img src={offer2} alt="" />
+        </div>
+      </div>
     </div>
-  )
+  );
 }
