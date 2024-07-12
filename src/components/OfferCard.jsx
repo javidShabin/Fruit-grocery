e917e9d695f099697fcf5efda0ce232d0f796852
javid shabin <javid.main.act@gmail.com>
2024-07-12 15:46:12 +0530

    Add css for offer card and align every items perfectly

--- src/components/OfferCard.jsx
+++ src/components/OfferCard.jsx
@@ -3,20 +3,20 @@ import { offer1, offer2, offerBg } from "../assets";
 
 export default function OfferCard() {
   return (
-    <div className="mt-9">
-      <div className="container grid grid-cols-2">
+    <div className="mt-9 px-[50px] ">
+      <div className="container flex justify-around">
         <div
-          className="bg-cover bg-center w-[600px] bg-orange-200 flex justify-between items-center px-5 rounded-md"
+          className="bg-cover bg-center w-[650px] bg-orange-200 flex justify-between items-center px-5 rounded-md"
           style={{ backgroundImage: `url(${offerBg})` }}
         >
           <div>
-            <label>Free Delivery</label>
-            <h2>Free delivery over $50</h2>
-            <p>
+            <label className="py-1 px-2 bg-orange-400 text-white leading-[50px]">Free Delivery</label>
+            <h2 className="text-[30px] text-blue-950">Free delivery over $50</h2>
+            <p className="text-gray-600">
               Shop $50 product and get free <br />
               delivery anywhere.
             </p>
-            <button>Shop Now</button>
+            <button className="py-1 px-2 bg-blue-600 text-white">Shop Now</button>
           </div>
           <div>
             <img src={offer1} alt="" />
@@ -24,17 +24,17 @@ export default function OfferCard() {
         </div>
 
         <div
-          className="bg-cover bg-center w-[600px] bg-green-200 flex justify-between items-center ounded-md"
+          className="bg-cover bg-center w-[650px] bg-green-200 flex justify-between items-center ounded-md"
           style={{ backgroundImage: `url(${offerBg})` }}
         >
           <div className="ml-5">
-            <label>60% Off</label>
-            <h2>Organic Food</h2>
-            <p>
+            <label className="py-1 px-2 bg-green-400 text-white leading-[50px]">60% Off</label>
+            <h2 className="text-[30px] text-blue-950">Organic Food</h2>
+            <p className="text-gray-600">
               Save up to 60% off on your <br />
               first order
             </p>
-            <button>Shop Now</button>
+            <button className="py-1 px-2 bg-blue-600 text-white">Shop Now</button>
           </div>
           <div className="mt-8">
             <img src={offer2} alt="" />
