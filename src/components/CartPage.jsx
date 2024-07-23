0e1566b4cd8cf667cd6075e6c9c2933b4aeaf9a5
javid shabin <javid.main.act@gmail.com>
2024-07-23 12:23:41 +0530

    Change bg color cart page

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -3,8 +3,8 @@ import React from "react";
 export default function CartPage() {
   return (
     <>
-      <div className="w-[400px] h-[600px] bg-[#d2fffe] text-center">
-        <h1 className="text-[20px] font-semibold">Cart Page</h1>
+      <div className="w-[400px] h-[600px] bg-[#3c3c3c] text-center">
+        <h1 className="text-[20px] text-[#d2d2d2] font-semibold">Cart Page</h1>
       </div>
     </>
   );
