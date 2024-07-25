b7ba29d4ec232ef18f62a8dd0c94b471af23694a
javid shabin <javid.main.act@gmail.com>
2024-07-25 15:20:45 +0530

    Add oreder buttin in cart page

--- src/components/CartPage.jsx
+++ src/components/CartPage.jsx
@@ -3,12 +3,12 @@ import React from "react";
 export default function CartPage({ cartItems }) {
   return (
     <>
-      <div className="w-[450px] h-[600px] bg-[#3c3c3c] text-center">
-        <h1 className="text-[20px] text-[#d2d2d2] font-semibold">Cart Page</h1>
-
+      <div className="w-[450px] h-[610px] bg-[#3c3c3c] text-center rounded-sm relative">
+        <h1 className="text-[20px] font-semibold border w-[200px] mx-auto bg-[#ffffff] text-black rounded-md">
+          Cart Page
+        </h1>
         <div className="mt-10 w-full px-1 relative">
           {cartItems.map((items) => {
-        
             return (
               <div className="flex justify-between items-center mt-3 py-3 px-2 shadow-xl bg-[#3b3b3b] w-full rounded-lg">
                 <img
@@ -22,6 +22,7 @@ export default function CartPage({ cartItems }) {
             );
           })}
         </div>
+        <button className="absolute bottom-3 left-3 bg-yellow-500 text-white rounded-md text-[15px] py-1 px-4">Order Now</button>
       </div>
     </>
   );
